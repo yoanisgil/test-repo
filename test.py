d236b89194ec6627418aa144df44583544a4d513
Yoanis Gil <gil.yoanis@gmail.com>
2023-02-13 15:44:16 -0500

    Add test.py

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("hello main")
+import sys
+import json
+
+
+if __name__ == "__main__":
+    assert len(sys.argv) > 1
+    json_object = json.load(open(sys.argv[1]))
+    print(json_object["name"])
+
